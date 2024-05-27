53bcf717a52a4a9b9070755fc33a78403b6b1075
davoszabo <davo.szabo@gmail.com>
2024-05-27 15:30:05 +0000

    Add new bufferline keybindings

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,12 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("bufferline").setup({})
+      
+      vim.keymap.set("n", "<A-l>", ":BufferLineCycleNext<CR>", {})
+      vim.keymap.set("n", "<A-h>", ":BufferLineCyclePrev<CR>", {})
+      vim.keymap.set("n", "<A-S-l>", ":BufferLineMoveNext<CR>", {})
+      vim.keymap.set("n", "<A-S-h>", ":BufferLineMovePrev<CR>", {})
+      vim.keymap.set("n", "<leader>bx", ":BufferLineCloseOthers<CR>", {})
     end,
   }
   
